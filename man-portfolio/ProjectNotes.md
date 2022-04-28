45440c94bf407b439407df30745c9d42682b24c4
Mancha01 <pamemmanuel0@gmail.com>
2022-04-28 22:39:49 +0100

    Penultimate

---
+++ man-portfolio/ProjectNotes.md
@@ -0,0 +1,2 @@
+//How to create a responsive mobile menu
+https://www.youtube.com/watch?v=P3W7MZ3JkyA
--- man-portfolio/src/components/Navbar.js
+++ man-portfolio/src/components/Navbar.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import { useState } from 'react'
+import '../styles/navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
+    const [isMobile, setIsMobile] = useState(false);
   return (
    
-        <nav>
+        <nav className='navbar'>
             <h1>Mancha Pam</h1>
             <div className="links">
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/projects'>Portfolio Projects</Link>
-                <Link to='/challenges'>Challenges</Link>
+                <ul className={isMobile? "nav-links-mobile": "nav-links"} onClick={() => setIsMobile(false)}>
+                    <Link to='/' className='the-links'>
+                        <li>Home</li>
+                    </Link>
+                    <Link to='/about' className='the-links'>
+                        <li>About</li>
+                    </Link>
+                    <Link to='/projects' className='the-links'>
+                        <li>Portfolio Projects</li>
+                    </Link>
+                    <Link to='/challenges' className='the-links'>
+                        <li>Challenges</li>
+                    </Link>
+                </ul>
+                <button className='mobile-menu-icon' onClick={() => setIsMobile(!isMobile)}>
+                    {isMobile ? (
+                 <FontAwesomeIcon icon={faTimes} />
+                    ) : (
+                        <FontAwesomeIcon icon={faBars} />
+                    )}
+                </button>
             </div>
         </nav>
 
@@ -19,3 +42,5 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+// https://www.youtube.com/watch?v=P3W7MZ3JkyA

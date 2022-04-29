d1adad45232dc3d1cc080e0e380640de13a720e5
Mancha01 <pamemmanuel0@gmail.com>
2022-04-29 18:33:41 +0100

    Final III

--- man-portfolio/gatsby-config.js
+++ man-portfolio/gatsby-config.js
@@ -7,6 +7,23 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-1LTZQ7BHYR", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
     'gatsby-transformer-remark',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,

e8c47928c00e01c632c064e0c9f0e1876d6e237d
Mancha01 <pamemmanuel0@gmail.com>
2022-05-01 17:44:07 +0100

    corrections II

--- man-portfolio/src/pages/challenges.js
+++ man-portfolio/src/pages/challenges.js
@@ -8,7 +8,7 @@ const Challenges = () => {
              <div>
                  
                  <div className='challenges'>
-                 <h1>Programming Challenges</h1>
+                 <h1>Programming Challenges</h1>  
                  <iframe width="560" height="315" src="https://www.youtube.com/embed/NLYtEo2KBN4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                  <hr />
                  <hr />

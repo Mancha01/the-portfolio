7ffa50a62d1ed49bd5658d0b1d6f20bf8d3bdd41
Mancha01 <pamemmanuel0@gmail.com>
2023-10-26 16:54:26 +0100

    resume updates and card animation

--- man-portfolio/src/pages/about.js
+++ man-portfolio/src/pages/about.js
@@ -1,65 +1,92 @@
-import React from 'react'
-import Layout from '../components/layout';
-import { FaGithub, FaLinkedinIn, FaTwitterSquare, FaMailBulk} from 'react-icons/fa';
-import '../styles/global.css'
-
+import React from "react"
+import Layout from "../components/layout"
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaTwitterSquare,
+  FaMailBulk,
+} from "react-icons/fa"
+import "../styles/global.css"
 
 const About = () => {
   return (
-      <Layout>
-    <div className='about'>
-    <p>
-            <hr />
-            My name is Emmanuel Pam Mancha and I am a FrontEnd Developer. I specialize in creating user interfaces that are 
-representative of a product idea/vision.
-
-<br />
-<br />
-
-I graduated from the University of Jos in 2019 with a Bachelor's Degree in Computer Science. While in school, I 
-earned the COMPUTER PROFESSIONALS CERTIFICATION, duly registered by the COMPUTER PROFESSIONALS REGISTRATION 
-COUNCIL OF NIGERIA. I've designed, developed, and deployed the quintessential high-value, high-quality user 
-interfaces which lead to highly engaging and incredibly performant products.
-
-<br />
-<br />
-
-I am wholly and exhaustively committed to the end-user. I am passionate about creating simple, seamless, and effective 
-online experiences that accentuate and ultimately climax the company offers. 
-
-<br />
-<br />
-
-<hr />
-
-<p>Tech Stack: HTML, CSS, JAVASCRIPT, SCSS, REACT, FIGMA, GATSBYJS, WEBFLOW, GIT</p>
-
-
-<hr />
-
-For more information about me, my work, and how to contact me, please consult the options below:
+    <Layout>
+      <div className="about">
+        <p>
+          <hr />
+          My name is Emmanuel Pam Mancha and I am a FrontEnd Developer. I
+          specialize in creating user interfaces that are representative of a
+          product idea/vision.
+          <br />
+          <br />
+          I graduated from the University of Jos in 2019 with a Bachelor's
+          Degree in Computer Science. While in school, I earned the COMPUTER
+          PROFESSIONALS CERTIFICATION, duly registered by the COMPUTER
+          PROFESSIONALS REGISTRATION COUNCIL OF NIGERIA. I've designed,
+          developed, and deployed the quintessential high-value, high-quality
+          user interfaces which lead to highly engaging and incredibly
+          performant products.
+          <br />
+          <br />
+          I am wholly and exhaustively committed to the end-user. I am
+          passionate about creating simple, seamless, and effective online
+          experiences that accentuate and ultimately climax the company offers.
+          <br />
+          <br />
+          <hr />
+          <p>
+            Tech Stack: HTML, CSS, JAVASCRIPT,TYPESCRIPT, REDUX,ANT DESIGN,
+            TAILWIND CSS, SCSS, REACT, FIGMA, GATSBYJS, WEBFLOW, GIT
+          </p>
+          <hr />
+          For more information about me, my work, and how to contact me, please
+          consult the options below:
         </p>
-    </div>
+      </div>
 
-    <div>
-    <a href="https://github.com/Mancha01" className='fa-icons' target='_blank'> 
-        <FaGithub />
-    </a>
-    <a href="https://www.linkedin.com/in/emmanuel-pam-081b89ab/" className='fa-icons' target='_blank'>  
-        <FaLinkedinIn />
-    </a>
-    <a href="https://twitter.com/EmmanuelPam9" className='fa-icons' target='_blank'> 
-        <FaTwitterSquare />
-    </a>
+      <div>
+        <a
+          href="https://github.com/Mancha01"
+          className="fa-icons"
+          target="_blank"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emmanuel-pam-081b89ab/"
+          className="fa-icons"
+          target="_blank"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a
+          href="https://twitter.com/EmmanuelPam9"
+          className="fa-icons"
+          target="_blank"
+        >
+          <FaTwitterSquare />
+        </a>
 
-    <a href = "mailto: pamemmanuel0@gmail.com" className='fa-icons' target='_blank'> 
-    <FaMailBulk />
-    </a>
-    
-    </div>
-    <hr />
+        <a
+          href="mailto: pamemmanuel0@gmail.com"
+          className="fa-icons"
+          target="_blank"
+        >
+          <FaMailBulk />
+        </a>
+      </div>
+      <hr />
+      <div className="download-button-container">
+        <a
+          href="https://drive.google.com/file/d/1YhF5Qt6hIzeW8cBtvc5PxSpmcOWRt-oi/view?usp=sharing"
+          className="download-button"
+          target="_blank"
+        >
+          My Resume
+        </a>
+      </div>
     </Layout>
   )
 }
 
-export default About;
+export default About
--- man-portfolio/src/pages/projects/index.js
+++ man-portfolio/src/pages/projects/index.js
@@ -1,10 +1,10 @@
 import { graphql, Link } from "gatsby"
 import React from "react"
+import { motion } from "framer-motion"
 import Layout from "../../components/layout"
 import * as containerStyles from "../../styles/projects.module.css"
 
 const Projects = ({ data }) => {
-  console.log(data)
   const projects = data.allMarkdownRemark.nodes
   return (
     <Layout>
@@ -13,80 +13,100 @@ const Projects = ({ data }) => {
         <h3>Projects & Websites I've Created</h3>
         <div className={containerStyles.projects}>
           <a href="https://my-music-app-livid.vercel.app/" target="_blank">
-            <div>
-              <img src="../Screenshot (2990).png" alt="My Music app" />
-              <h3>My Music App</h3>
-              <p>Next js</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (2990).png" alt="My Music app" />
+                <h3>My Music App</h3>
+                <p>Next js</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://todo-app-8ic.pages.dev/" target="_blank">
-            <div>
-              <img src="../Screenshot (4103).png" alt="Todo App" />
-              <h3>Todo App</h3>
-              <p>React, Redux</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (4103).png" alt="Todo App" />
+                <h3>Todo App</h3>
+                <p>React, Redux</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://shopping-cart-dmp.pages.dev/" target="_blank">
-            <div>
-              <img src="../Screenshot (4006).png" alt="Shopping Cart" />
-              <h3>Shopping Cart</h3>
-              <p>React, Redux Toolkit</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (4006).png" alt="Shopping Cart" />
+                <h3>Shopping Cart</h3>
+                <p>React, Redux Toolkit</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://real-estate-land-b9n5.vercel.app/" target="_blank">
-            <div>
-              <img
-                src="../Screenshot (2992).png"
-                alt="Real Estate Land Project"
-              />
-              <h3>Real Estate Land</h3>
-              <p>Next js</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img
+                  src="../Screenshot (2992).png"
+                  alt="Real Estate Land Project"
+                />
+                <h3>Real Estate Land</h3>
+                <p>Next js</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://ecommerce-v2.pages.dev" target="_blank">
-            <div>
-              <img src="../Screenshot (3285).png" alt="ecommerce crud v2" />
-              <h3>Ecommerce CRUD test project version ii</h3>
-              <p>React</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (3285).png" alt="ecommerce crud v2" />
+                <h3>Ecommerce CRUD test project version ii</h3>
+                <p>React</p>
+              </div>
+            </motion.div>
           </a>
           <a
             href="https://ecommerce-crud-test-project.pages.dev/"
             target="_blank"
           >
-            <div>
-              <img src="../Screenshot (3254).png" alt="ecommerce crud" />
-              <h3>Ecommerce CRUD test project</h3>
-              <p>React</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (3254).png" alt="ecommerce crud" />
+                <h3>Ecommerce CRUD test project</h3>
+                <p>React</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://the-dash.pages.dev/" target="_blank">
-            <div>
-              <img src="../Screenshot (991).png" alt="the dash" />
-              <h3>The Dash</h3>
-              <p>Html, Css and Javascript</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (991).png" alt="the dash" />
+                <h3>The Dash</h3>
+                <p>Html, Css and Javascript</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://the-bank.pages.dev/" target="_blank">
-            <div>
-              <img src="../Screenshot (990).png" alt="the bank" />
-              <h3>The Bank</h3>
-              <p>Html, Css and Javascript</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (990).png" alt="the bank" />
+                <h3>The Bank</h3>
+                <p>Html, Css and Javascript</p>
+              </div>
+            </motion.div>
           </a>
           <a href="https://the-eclipse-618f27.webflow.io/" target="_blank">
-            <div>
-              <img src="../Screenshot (1017).png" alt="The Eclipse" />
-              <h3>The Eclipse</h3>
-              <p>Webflow</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (1017).png" alt="The Eclipse" />
+                <h3>The Eclipse</h3>
+                <p>Webflow</p>
+              </div>
+            </motion.div>
           </a>
           <Link to="/blog">
-            <div>
-              <img src="../Screenshot (994).png" alt="the blog" />
-              <h3>The Blog</h3>
-              <p>React</p>
-            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <div>
+                <img src="../Screenshot (994).png" alt="the blog" />
+                <h3>The Blog</h3>
+                <p>React</p>
+              </div>
+            </motion.div>
           </Link>
         </div>
       </div>
